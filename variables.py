da96dc327281c672acd00dab5f9255399145ba31
BryanMoreta <bmcraft145@gmail.com>
2025-09-29 16:09:50 -0500

    sintaxis basica

---
+++ variables.py
@@ -0,0 +1,53 @@
+a = 2
+b = 3
+
+rname= "MORETA"
+
+is_active = True
+
+print(type(a))
+print(type(b))
+print(type(rname))
+
+device = 'router'
+id = 101
+
+
+number1 = 4
+number2 = 2
+
+
+
+# result = number1 + number2
+# print("la suma es: ", result)
+
+print("El resultado es: ", number1+number2)  #suma
+print("El resultado es: ", number1-number2)  #resta
+print("El resultado es: ", number1*number2)  #multiplicacion
+print("El resultado es: ", number1/number2)  #division
+print("El resultado es: ", number1**number2)  #potencia
+
+
+#STRINGS
+print("string")
+
+name = "bryan moreta"
+
+print(name.upper())
+print(name.capitalize())
+print(name.lower())
+
+lenguaje='PYTHON'
+print(lenguaje[0])
+print(lenguaje[-1])
+
+print(len(lenguaje))
+
+#LISTA}
+
+print("LISTA")
+device=("ruta", 2, True)
+
+print(len(device))
+print(device[0])
+device.append('Server')
